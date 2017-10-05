2203b5c71235f39f6fbff741fdfbb841c8988e99
fchristiansen <felipe@seo2.cl>
2017-10-05 16:42:00 -0300

    ajustes WP
    - se elimino legales concursos de footer
    - se eliminó secciones novedades, patio comida , turistas

--- include-top.php
+++ include-top.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // TRADUCCIONES
 if(ICL_LANGUAGE_CODE=='en'){
     $menu2 = "Luxury district";
@@ -11,7 +11,7 @@ if(ICL_LANGUAGE_CODE=='en'){
     $menu9 = "Revista detalle - Magazine";
     $menu10 = "Customer service";
     $menu11 = "travellers";
-    
+
 	$tit1 = "Time schedules ";
 	$tit2 = "How to get there ";
 	$tit3 = "Subscribe here";
@@ -19,7 +19,7 @@ if(ICL_LANGUAGE_CODE=='en'){
 	$tit5 = "Enter your email";
 	$btn1 = "View all our stores";
 	$btn2 = "View more";
-}elseif(ICL_LANGUAGE_CODE=='pt-br'){ 
+}elseif(ICL_LANGUAGE_CODE=='pt-br'){
     $menu2 = "Distrito de luxo";
     $menu3 = "boulevard";
     $menu4 = "Andar de desenho";
@@ -30,7 +30,7 @@ if(ICL_LANGUAGE_CODE=='en'){
     $menu9 = "Revista detalhe";
     $menu10 = "Serviço ao cliente";
     $menu11 = "Viajantes";
-    
+
 	$tit1 = "Horários";
 	$tit2 = "Como chegar";
 	$tit3 = "Inscreva-se";
@@ -38,7 +38,7 @@ if(ICL_LANGUAGE_CODE=='en'){
 	$tit5 = "Digite o seu e-mail";
 	$btn1 = "Veja todas as nossas lojas";
 	$btn2 = "Veja mais";
-}else{ 	
+}else{
     $menu3 = "Patio de Comidas";
     $menu4 = "Bazar Central";
     $menu5 = "Turistas";
@@ -46,7 +46,7 @@ if(ICL_LANGUAGE_CODE=='en'){
     $menu7 = "tiendas";
 	$menu8 = "Galería";
     $menu10 = "servicio al cliente";
-    
+
 	$tit1 = "horarios";
 	$tit2 = "cómo llegar";
 	$tit3 = "suscríbete";
@@ -54,16 +54,16 @@ if(ICL_LANGUAGE_CODE=='en'){
 	$tit5 = "ingresa tu email";
 	$btn1 = "ver todas las tiendas";
 	$btn2 = "ver más";
-	
-} ?> 
-<?php 
+
+} ?>
+<?php
     $detect = new Mobile_Detect();
 ?>
 
      <section class="top">
-		<?php 
-			if (!$detect->isMobile()) { 	  
-		?>  
+		<?php
+			if (!$detect->isMobile()) {
+		?>
         <nav id="nav_desktop" class="navbar navbar-default navbar-fixed-top bg_blanco">
          <div class="nav_bottom_line"></div>
            <div class="container width">
@@ -80,21 +80,21 @@ if(ICL_LANGUAGE_CODE=='en'){
 
 <!--
                     <div id="lang_switcher" class="visible-xs visible-sm">
-                       <select name="language" class="selectpicker show-menu-arrow"> 
+                       <select name="language" class="selectpicker show-menu-arrow">
                             <option selected="" value="/esp/">ESP</option>
                             <option value="/eng/">ENG</option>
                              <option value="/por/">POR</option>
                       </select>
-                         
-                      </div>  
+
+                      </div>
 
                   <div class="lang ">
                     <ul class="hidden-xs hidden-sm">
 						<?php icl_post_languages(); ?>
                     </ul>
-                   </div>  
+                   </div>
 -->
-                   
+
                 <div class="rrss hidden-xs hidden-sm">
                    <ul>
                      <li><a href="https://www.facebook.com/araucocoronel/" target="_blank"><i class="fa fa-facebook hvr-float" aria-hidden="true"></i></a></li>
@@ -104,28 +104,28 @@ if(ICL_LANGUAGE_CODE=='en'){
 
 					 <li><a href="<?php bloginfo('site_url'); ?>/horarios"><i class="fa fa-clock-o hvr-float" aria-hidden="true"></i></a></li>
                    </ul>
-                </div> 
-                <!-- RRSS -->  
+                </div>
+                <!-- RRSS -->
 
 
               <a class="close_tienda" href="javascript:void(0);" data-id="<?php echo $_GET['tiendaID']; ?>"> </a>
              </div> <!-- navbar header -->
-          
+
           <div id="navbar" class="navbar-collapse collapse ">
                <div class="container menu_container">
                   <div class="divisor_v"></div>
                   <div class="row">
-                      <div class="col-sm-12 col-md-6"> 
+                      <div class="col-sm-12 col-md-6">
                           <!-- col izquierda  -->
                           <div class="row">
                             <div class="col-sm-12 col-md-6">
                                  <ul class="nav navbar-nav">
                                      <li class="active"><a 			href="<?php bloginfo('url'); ?>">Home</a></li>
-                                     <li><a id="btn_tiendas" 		href="<?php bloginfo('url'); ?>/tiendas#tiendas"><?php echo $menu7; ?></a></li>
-                                     <li><a id="btn_boulevard" 		href="<?php bloginfo('url'); ?>/food-court-tiendas#tiendas"><?php echo $menu3; ?></a></li>
+                               <!--       <li><a id="btn_tiendas" 		href="<?php bloginfo('url'); ?>/tiendas#tiendas"><?php echo $menu7; ?></a></li> -->
+                                   <!--   <li><a id="btn_boulevard" 		href="<?php bloginfo('url'); ?>/food-court-tiendas#tiendas"><?php echo $menu3; ?></a></li> -->
 <!--                                      <li><a id="btn_piso" 			href="<?php bloginfo('url'); ?>/bazar-central"><?php echo $menu4; ?></a></li> -->
-                                     <li><a id="btn_entretencion" 	href="<?php bloginfo('url'); ?>/turistas"><?php echo $menu5; ?></a></li>
-                                     <li><a id="btn_novedades" 		href="<?php bloginfo('url'); ?>/novedades#novedades"><?php echo $menu6; ?></a></li>
+                                    <!--  <li><a id="btn_entretencion" 	href="<?php bloginfo('url'); ?>/turistas"><?php echo $menu5; ?></a></li> -->
+                                    <!--  <li><a id="btn_novedades" 		href="<?php bloginfo('url'); ?>/novedades#novedades"><?php echo $menu6; ?></a></li> -->
                                      <li><a href="<?php bloginfo('url'); ?>/servicio-al-cliente"><?php echo $menu10; ?></a></li>
                                 </ul>
                             </div>
@@ -148,7 +148,7 @@ if(ICL_LANGUAGE_CODE=='en'){
                       <div class="col-sm-12 col-md-6">
                       <!-- col derecha -->
                           <div class="row">
-                            <div class="col-md-6"> 
+                            <div class="col-md-6">
                             <!--      <div class="faq">
                                         <a href="">
                                           preguntas <i class="fa fa-comments-o" aria-hidden="true"> </i><br>
@@ -170,8 +170,8 @@ if(ICL_LANGUAGE_CODE=='en'){
                             <div class="col-md-6"></div>
                         </div> <!-- row -->
 						<div class="row">
-							<div class="col-sm-12 col_box_news_letter"> 
-								<div class="box_newsletter" style="background: url(<?php bloginfo('template_url'); ?>/assets/img/bg_box_newsletter.jpg);">  
+							<div class="col-sm-12 col_box_news_letter">
+								<div class="box_newsletter" style="background: url(<?php bloginfo('template_url'); ?>/assets/img/bg_box_newsletter.jpg);">
 									<h3><?php echo $tit3; ?></h3>
 									<form class="form_neswsletter" role="" id="form_neswsletter">
 										<div class="box_input">
@@ -193,14 +193,14 @@ if(ICL_LANGUAGE_CODE=='en'){
                       </div><!-- col -->
                   </div><!-- row -->
                </div><!-- container -->
-                
+
              </div><!--/.nav-collapse -->
            </div>
-         </nav> <!-- nav desktop-->          
+         </nav> <!-- nav desktop-->
         <?php } ?>
-		<?php 	 
-			if ($detect->isMobile()) { 
-			   include('include-nav-mobile.php'); 
+		<?php
+			if ($detect->isMobile()) {
+			   include('include-nav-mobile.php');
 			}
 		?>
      </section> <!-- top -->
